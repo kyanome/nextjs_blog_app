53a5e515c236230f219818f1d8d8ced5a04bdae4
kyanome <yanome.keita@gmail.com>
2025-03-27 08:13:08 +0900

    fix : useSWRの返り値を全て返せるように修正

--- src/hooks/useDataFetch.tsx
+++ src/hooks/useDataFetch.tsx
@@ -1,12 +1,4 @@
 import api from "@/utils/api";
 import useSWR from "swr";
 
-export const useDataFetch = (path: string) => {
-  const { data, error, isLoading, mutate } = useSWR(path, api.get);
-  return {
-    data,
-    error,
-    isLoading,
-    mutate,
-  };
-};
+export const useDataFetch = (path: string) => useSWR(path, api.get);
